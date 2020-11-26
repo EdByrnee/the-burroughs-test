3668e885c70a70de9d5f3867cf1c8b44b635891e
Ed Byrne <ed_b@hotmail.co.uk>
2020-11-26 11:10:07 +0000

    changes

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface Message {
   message: string;
+}
+
+export class ListEmployeesResponseDto{
+
+  @ApiProperty({ type: String, isArray: true, example: "emp1" })
+  employees: any[];
+
 }
